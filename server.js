77958932646c06466e39e59d21904498bc41787b
David Alvarado <mora21153@gmail.com>
2021-07-16 22:28:01 -0500

    final

--- server.js
+++ server.js
@@ -2,48 +2,86 @@ const { notStrictEqual } = require('assert');
 const { request, response } = require('express');
 const express = require('express');
 const path = require("path");
-const app = express () // app contiene las propiedades de express
+const app = express ()
 const PORT = 8000
 
-//Midedelwares metos para respomder a las peticiones del cliente
-// Los Midedelwares trabajara en las peticiones  (GET, POST, PUT , DELETE)
-//Middelwares incorporado (built-in) para respondes peteciones de estilos
+
+
+//Configuracion de Ejs
+app.set('views', path.join(__dirname, 'views'));
+//Definir el motor de plantilas
+app.set('view engine', 'ejs')
 
 let visitas = 1
-let visit  = app.use((response, request , next)=>{
-    function counter (){
-        console.log(visitas ++)   
-    }
-   /*  if (visitas === 5){
-       return next(new Error("El número de sesiones de tu dominio estan siendo ocupadas"));
-    } */
-    next()
-    counter(visitas)
-})
-
-app.use(express.static(path.join(__dirname, "Public")));
-app.use(express.urlencoded()); // permite recibir los datos enviados desde el cliente
-app.use(express.json());// procesar datos atravez de formato json
-
 app.get("/", (request, response)=>{
-    let pathHome = path.join(__dirname, "Public", "index.html") // entregar la ruta del archivo
-    response.sendFile(pathHome)
-})
-// metodo para redireccionar con express
-app.get("/shop",(request,response)=>{ 
-    response.redirect("/")
+        console.log(visitas ++)
+  response.render("pages/home", {title:"inicio", message : "Hola mundo con EJS", asistencia: visitas})
 })
 
-
-//metodo para recibir los datos del clie
-app.post("/registro",(request, response)=>{
-    const user = request.body;
-    console.log(request.headers)
-    console.log(user)
+app.get("/tareas", (request, response)=>{
+    let taskArray = [{
+        "id": 1,
+        "title": "Blue-faced booby",
+        "description": "Occlusion of Pancreatic Duct, Percutaneous Approach"
+      }, {
+        "id": 2,
+        "title": "Gazelle, thomson's",
+        "description": "Revision of Drain Dev in R Toe Phalanx Jt, Extern Approach"
+      }, {
+        "id": 3,
+        "title": "Partridge, coqui",
+        "description": "Monitoring of Temperature, External Approach"
+      }, {
+        "id": 4,
+        "title": "Rhinoceros, white",
+        "description": "LDR Brachytherapy of Jejunum using Iridium 192"
+      }, {
+        "id": 5,
+        "title": "Owl, white-browed",
+        "description": "Plaque Radiation of Head and Neck"
+      }, {
+        "id": 6,
+        "title": "Squirrel, indian giant",
+        "description": "Extirpation of Matter from Ascending Colon, Perc Approach"
+      }, {
+        "id": 7,
+        "title": "Gecko (unidentified)",
+        "description": "Destruction of Sacrococcygeal Joint, Open Approach"
+      }, {
+        "id": 8,
+        "title": "Tapir, brazilian",
+        "description": "Revision of Autol Sub in Stomach, Extern Approach"
+      }, {
+        "id": 9,
+        "title": "Heron, green",
+        "description": "Supplement Right Neck Muscle with Autol Sub, Open Approach"
+      }, {
+        "id": 10,
+        "title": "Springbok",
+        "description": "Removal of Synthetic Substitute from L Carpal, Open Approach"
+      }, {
+        "id": 11,
+        "title": "Beaver, european",
+        "description": "Excision of Left Tarsal, Perc Endo Approach, Diagn"
+      }, {
+        "id": 12,
+        "title": "Skunk, western spotted",
+        "description": "Inspection of Left Acromioclavicular Joint, Extern Approach"
+      }, {
+        "id": 13,
+        "title": "Red and blue macaw",
+        "description": "Reposition Left Patella with Ext Fix, Perc Endo Approach"
+      }, {
+        "id": 14,
+        "title": "Great white pelican",
+        "description": "Drainage of Sacrum with Drainage Device, Perc Endo Approach"
+      }, {
+        "id": 15,
+        "title": "Black-backed magpie",
+        "description": "Transfer R Foot Subcu/Fascia with Skin, Subcu, Open Approach"
+      }]
+    response.render("pages/task", {title:"Tasks", message : "Lista de Tareas", items: taskArray})
 })
-
-
-
 
 app.use((request, response)=>{
     let pathNotFound = path.join(__dirname, "Public", "404.html")
@@ -54,4 +92,5 @@ app.use((request, response)=>{
 app.listen(PORT, ()=> { 
     console.log(`el servidor esta escuchando en el puerto : ${PORT}`)
 })
+
 
